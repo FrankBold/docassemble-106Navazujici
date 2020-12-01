3ceee8619613b2fe8ac8a4190fef757d65c6c19a
Michal Kuk <michal.kuk@frankbold.org>
2020-12-01 17:25:03 +0100

    Doplnění

--- docassemble/106Navazujici/funkce.py
+++ docassemble/106Navazujici/funkce.py
@@ -9,3 +9,13 @@ def ziskejSituace():
   for x in y["Situace"]:
     dict[x["ID"]] = x["title"]
   return dict
+
+def ziskejDuvody(parent):
+  page = requests.get("https://da-test.frankbold.org/playgroundstatic/PravoNaInformace/1/situace.json")
+  y = json.loads(page.content)
+  dict = {}
+
+  for x in y["Duvody"]:
+    if x["parent"] == parent and x["v"] == 1:
+      dict[x["ID"]] = x["title"]
+  return dict
