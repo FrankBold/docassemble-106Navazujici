7e6a5213a7665fbe9e2967bb958809166eb1a0a4
Michal Kuk <michal.kuk@frankbold.org>
2020-12-01 18:09:08 +0100

    ..

--- docassemble/106Navazujici/funkce.py
+++ docassemble/106Navazujici/funkce.py
@@ -16,6 +16,16 @@ def ziskejDuvody(parent):
   dict = {}
 
   for x in y["Duvody"]:
-    if x["parent"] == parent and x["v"] == 1:
+    if x["parent"] == int(parent) and x["v"] == 1:
       dict[x["ID"]] = x["title"]
+  return dict
+
+def ziskejArgumentyProti(parent):
+  page = requests.get("https://da-test.frankbold.org/playgroundstatic/PravoNaInformace/1/argumenty.json")
+  y = json.loads(page.content)
+  dict = {}
+
+  for x in y["Argumenty"]:
+    if x["parent"] == int(parent):
+      dict[x["ID"]] = x["titleMinus"]
   return dict
